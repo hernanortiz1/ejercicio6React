fb1819f79b60c6dbbc21a502db424a53ddaf5863
Hernan Ortiz <hernannicolasortiz@gmail.com>
2025-06-15 12:07:30 -0300

    agregue titulo y baje footer en App

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+
 function App() {
-  return <>
-    <h1>Administrar Colores</h1>   
-  </>;
+  return (
+    <>
+      <main className="container my-4">
+        <h1 className="text-center">Administrar Colores</h1>
+      </main>
+      <footer className="bg-dark text-light text-center py-3">
+        &copy; Todos los derechos reservados
+      </footer>
+    </>
+  );
 }
 
 export default App;
