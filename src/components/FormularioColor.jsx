8bb6d901d3bca02ae59c0640a67b9a942797952c
Hernan Ortiz <hernannicolasortiz@gmail.com>
2025-06-15 13:20:38 -0300

    agregue contenido a CuadroColor y centre contenido

--- src/components/FormularioColor.jsx
+++ src/components/FormularioColor.jsx
@@ -1,19 +1,32 @@
 import { Form, Button } from "react-bootstrap";
+import CuadroColor from "./CuadroColor";
 
 const FormularioColor = () => {
   return (
     <>
-      <section>
-        <Form>
-          <Form.Group className="mb-3">
-            <Form.Control type="text" placeholder="Ingresa un color ej: Blue" />
-            <div className="text-end ">
-              <Button type="submit" variant="primary" className="m-3 px-4 shadow-sm">
-                Guardar
-              </Button>
-            </div>
-          </Form.Group>
-        </Form>
+      <section className="row px-2">
+        <div className=" col-4 col-md-2">
+          <CuadroColor />
+        </div>
+        <div className="col-8 col-md-10">
+          <Form>
+            <Form.Group className="ms-2 mb-3 mt-md-3">
+              <Form.Control
+                type="text"
+                placeholder="Ingresa un color ej: Blue"
+              />
+              <div className="text-end ">
+                <Button
+                  type="submit"
+                  variant="primary"
+                  className="m-3 px-4 shadow-sm"
+                >
+                  Guardar
+                </Button>
+              </div>
+            </Form.Group>
+          </Form>
+        </div>
       </section>
     </>
   );
