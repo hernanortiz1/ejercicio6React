abf5e1ff5256224048b49f96cf2876a8ddd144fc
Hernan Ortiz <hernannicolasortiz@gmail.com>
2025-06-15 12:31:25 -0300

    agregue clase para App y btn a Formulario Color

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import FormularioColor from "./components/FormularioColor";
 
 function App() {
   return (
     <>
       <main className="container my-4">
-        <h1 className="text-center">Administrar Colores</h1>
+        <section className="shadow px-5 fondoPrincipal rounded-4">
+          <h1 className="text-center py-3">Administrar Colores</h1>
+          <FormularioColor />
+        </section>
       </main>
       <footer className="bg-dark text-light text-center py-3">
         &copy; Todos los derechos reservados
--- src/components/FormularioColor.jsx
+++ src/components/FormularioColor.jsx
@@ -1,11 +1,22 @@
-
+import { Form, Button } from "react-bootstrap";
 
 const FormularioColor = () => {
-    return (
-        <>
-       
-        </>
-    );
+  return (
+    <>
+      <section>
+        <Form>
+          <Form.Group className="mb-3">
+            <Form.Control type="text" placeholder="Ingresa un color ej: Blue" />
+            <div className="text-end ">
+              <Button type="submit" variant="primary" className="m-3 px-4 shadow-sm">
+                Guardar
+              </Button>
+            </div>
+          </Form.Group>
+        </Form>
+      </section>
+    </>
+  );
 };
 
 export default FormularioColor;
