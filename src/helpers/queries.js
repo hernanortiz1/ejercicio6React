b0c95747bd40166369a5f2ccbc190b622f676325
Hernan Ortiz <hernannicolasortiz@gmail.com>
2025-08-31 02:18:53 -0300

    agregue funcion borrar

--- src/helpers/queries.js
+++ src/helpers/queries.js
@@ -52,3 +52,14 @@ export const editarColor = async (colorEditado, id) => {
     }
 };
 
+export const borrarColorPorID = async (id) => {
+    try{
+        const respuesta = await fetch(urlColor+ `/${id}`,{
+            method: 'DELETE',
+        })
+        return respuesta
+    }catch(error){
+        console.error(error);
+        return null
+    }
+};
