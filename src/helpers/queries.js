605c9a64029238d166c5c116798072bd2ae3fe2f
Hernan Ortiz <hernannicolasortiz@gmail.com>
2025-08-31 02:18:25 -0300

    agregue funcion editar

--- src/helpers/queries.js
+++ src/helpers/queries.js
@@ -10,3 +10,45 @@ export const leerColor = async () => {
     }
 };
 
+export const obtenerColorPorID = async (id) => {
+    try{
+        const respuesta = await fetch(urlColor+ `/${id}`)
+        return respuesta
+    }catch(error){
+        console.error(error);
+        return null
+    }
+};
+
+export const crearColor = async (colorNuevo) => {
+    try{
+        const respuesta = await fetch(urlColor,{
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(colorNuevo)
+        })
+        return respuesta
+    }catch(error){
+        console.error(error);
+        return null
+    }
+};
+
+export const editarColor = async (colorEditado, id) => {
+    try{
+        const respuesta = await fetch(urlColor+ `/${id}`,{
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(colorEditado)
+        })
+        return respuesta
+    }catch(error){
+        console.error(error);
+        return null
+    }
+};
+
